1f8ca12df8f31151016c99fad85e18371d6f7385
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-05-04 08:48:53 -0300

    commit thell da aula 2508

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -24,7 +24,21 @@ public class CalculadoraSimples{
             op = entrada.nextInt();
 
             if(op == 1){
+                //Thell
                 System.out.println("\n\t\t\t -- Soma --\n");
+
+                //Entrada
+                System.out.println("Informe N1: ");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2: ");
+                n2 = entrada.nextInt();
+
+                //Processamento
+                total = n1 + n2;
+
+                //Saída
+                System.out.printf(format:"%d + $d = %d" , n1, n2, total);
+
             } else if(op == 2){
                 System.out.println("\n\t\t\t -- Subtração -- \n");
             } else if(op == 3){
